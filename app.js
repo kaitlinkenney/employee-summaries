2f81a60483210a901db5f23b3856f5441eb6503e
kaitlinkenney <kaitlin.kenney96@gmail.com>
2021-02-10 10:58:22 -0500

    tests passing

--- app.js
+++ app.js
@@ -14,6 +14,113 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+let arr = [];
+
+function start() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "role",
+                message: "What is the employee's job role?",
+                choices: ["Engineer", "Manager", "Intern"]
+            }
+        ])
+        .then(function (answer) {
+            if (answer.role === "Engineer") {
+                askEngineer();
+            }
+            else if (answer.role === "Manager") {
+                askManager();
+            }
+            else if (answer.role === "Intern") {
+                askIntern();
+            }
+        })
+}
+
+function buildTeam(){
+    
+}
+
+function askEngineer() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "engName",
+                message: "What is the employee's name?"
+            },
+            {
+                type: "input",
+                name: "engEmail",
+                message: "What is the employee's email?"
+            },
+            {
+                type: "input",
+                name: "engGithub",
+                message: "What is the employee's GitHub username?"
+            }
+        ])
+        .then(function (answer){
+let eng = new Engineer(answer.engName, answer.engEmail, answer.engGithub);
+arr.push(eng);
+        })
+}
+
+function askManager() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "mgrName",
+                message: "What is the manager's name?"
+            },
+            {
+                type: "input",
+                name: "mgrEmail",
+                message: "What is the manager's email?"
+            },
+            {
+                type: "input",
+                name: "mgrOfficeNum",
+                message: "What is the manager's office phone number?"
+            }
+        ])
+        .then(function (answer){
+          push  
+        })
+}
+
+function askIntern() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "intName",
+                message: "What is the intern's name?"
+            },
+            {
+                type: "input",
+                name: "intEmail",
+                message: "What is the intern's email?"
+            },
+            {
+                type: "input",
+                name: "intSchool",
+                message: "What school does the intern attend?"
+            }
+        ])
+        .then(function (){
+            // const engineer = new Engineer(16, 'sailboat', boatPassengers);
+            render();
+            (err, answer) => {
+                if (err) throw err;
+                fs.writeFileSync (outputPath, answer);
+            }
+        })
+}
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
@@ -23,10 +130,6 @@ const render = require("./lib/htmlRenderer");
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
 
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
